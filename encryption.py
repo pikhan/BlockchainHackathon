914bb11a807b2d0f4e3595179f11fe65588413b1
ilyakuznetsov <ilyakuznetsov.code@gmail.com>
2019-11-10 09:28:14 -0800

    Added GUI

--- encryption.py
+++ encryption.py
@@ -4,6 +4,7 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 
+
 # key class
 class HackathonKeyBlock:
     def __init__(self):
